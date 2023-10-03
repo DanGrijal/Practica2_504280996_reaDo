1496c305cadb9496cdb7e27af5dbbb047d83e99d
Danny Enrique Grijalba Gutiérrez <danny.grijal1999@outlook.com>
2023-10-02 22:05:22 -0600

    Práctica Semana 2 - Elementos visuales: Pagina web personal

---
+++ src/main/java/com/reaDo/reaDo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.reaDo.reaDo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
